1b3e04f0b4797075cf6ce10d14027336431b61bd
CarismaDAqueen <130988079+ccousins694@users.noreply.github.com>
2023-05-11 17:28:03 -0400

    Add palindrome function for exercise 4

--- modern-javascript-for-react-devs/students/2/main.js
+++ modern-javascript-for-react-devs/students/2/main.js
@@ -21,6 +21,12 @@ const ex3 = () => {
     console.log(interleave(array1, array2));
  }  
 
+ const ex4 = () => {
+    console.log(palindrome('radar'));
+    console.log(palindrome('month'));
+}
+
+
 //
 // Your functions here...
 //
@@ -73,11 +79,44 @@ const interleave = (ray1, ray2) => {
 }
 
 
+    // ex4
+    const palindrome = (str) => {
+        //splitStr takes original string and splits it
+        const splitStr = str.split("")     
+        //reverseStr takes splits string and reverses it
+        const reverseStr = splitStr.reverse("")
+        //joinStr takes reverse string and joins it 
+        const newStr = reverseStr.join("")
+
+        if(str === newStr){
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    //this is my function (below)
+    // const palindrome = (str) => {
+    //     // in the for loop, inside the parenthesis are the conditions; they include how loop should start and length of loop and incremementing loop
+    //     for (let i = 0; i < str.length; i++){
+    //         //do something...
+    //         // console.log("forward: ", str[i])
+    //         // console.log("backward: ", str[str.length-1-i])
+    //         if(str[i] === str[str.length-1-i]){
+    //             return true
+    //         }else {
+    //             return false
+    //         }
+    //     }
+
+    // }
+
 
 const main = async () => {
     // ex1();
     // ex2();
-    ex3();
+    // ex3();
+    ex4();
 }
 
 main();
